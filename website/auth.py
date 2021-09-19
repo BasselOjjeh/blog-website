43eb535a4d5609b661e12bbd67039b7df33a4b4e
BasselOjjeh <90906033+BasselOjjeh@users.noreply.github.com>
2021-09-19 17:54:15 +0100

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
-
+#Setting Up Blueprints using Jinja
 @auth.route("/login")
 def login():
     return render_template("login.html")
