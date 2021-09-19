f8bdd5ea43539000b8e1f447dbbc07fcb1d86514
BasselOjjeh <90906033+BasselOjjeh@users.noreply.github.com>
2021-09-19 17:53:26 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-
+#Creating the app and importing routes
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
