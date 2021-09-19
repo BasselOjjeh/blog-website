a62ec2f829e40eabb468854c655ed57fa45edff5
BasselOjjeh <90906033+BasselOjjeh@users.noreply.github.com>
2021-09-19 18:04:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from website import create_app
-
+#Setting the website on local host 5000 using flask by importing a sub-package
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
